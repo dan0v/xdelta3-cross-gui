5c0b9ff6fa8074ddb39db9734d3930b79c2e8cd4
dan0v <7658521+dan0v@users.noreply.github.com>
2020-08-20 14:10:49 +0200

    Add macOS .app publishing script

---
+++ Publish/MacOS/MacOS.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+APP_NAME="xDelta3 Cross GUI.app"
+APP_OUTPUT_PATH="Output"
+APP_TAR_NAME="xdelta3_cross_gui_macOS_x64"
+PUBLISH_OUTPUT_DIRECTORY="../../bin/Release/netcoreapp3.1/publish/."
+INFO_PLIST="Info.plist"
+ICON_FILE="Icon.icns"
+
+if [ -d "$APP_OUTPUT_PATH" ]
+then
+    rm -rf "$APP_OUTPUT_PATH"
+fi
+
+mkdir "$APP_OUTPUT_PATH"
+mkdir "$APP_OUTPUT_PATH/$APP_NAME"
+
+mkdir "$APP_OUTPUT_PATH/$APP_NAME/Contents"
+mkdir "$APP_OUTPUT_PATH/$APP_NAME/Contents/MacOS"
+mkdir "$APP_OUTPUT_PATH/$APP_NAME/Contents/Resources"
+
+cp "$INFO_PLIST" "$APP_OUTPUT_PATH/$APP_NAME/Contents/Info.plist"
+cp "$ICON_FILE" "$APP_OUTPUT_PATH/$APP_NAME/Contents/Resources/$ICON_FILE"
+cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_OUTPUT_PATH/$APP_NAME/Contents/MacOS"
+chmod +x "$APP_OUTPUT_PATH/$APP_NAME/Contents/MacOS/xdelta3_cross_gui"
+cd "$APP_OUTPUT_PATH"
+tar -czvf "$APP_TAR_NAME.tar.gz" "$APP_NAME"
